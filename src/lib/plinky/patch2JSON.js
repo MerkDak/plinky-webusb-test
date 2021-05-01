53edd4e69ce01adaad61e711987effd07c027e8e
Orangetronic <david@orangetronic.com>
2021-05-01 22:19:31 +0100

    Adds Param and Patch classes backed by ArrayBuffer
    these remain backed by the same array buffer you use to instantiate them, so any changes made via these classes can be written back to plinky by sending back the same ArrayBuffer.
    
    these are API compatible with the previous patch json structure, so there needn't be any changes in the front end, I don't think. 
    
    i'm a bit unclear on how this interacts with svelte's reactive bits, but so far seems to work OK!

--- src/lib/plinky/patch2JSON.js
+++ src/lib/plinky/patch2JSON.js
@@ -1,14 +1,131 @@
 import { EParams, PlinkyParams } from './params';
 
 function getParam(id) {
-  console.log(PlinkyParams);
   return PlinkyParams.find(param => {
     return param.id === id;
   });
 }
 
+class Param {
+
+  constructor(params) {
+    Object.assign(this, params);
+    this.mods = {
+      get env() {
+        return params.arr[1];
+      },
+      set env(val) {
+        params.arr[1] = val;
+      },
+      get pressure() {
+        return params.arr[2];
+      },
+      set pressure(val) {
+        params.arr[2] = val;
+      },
+      get a() {
+        return params.arr[3];
+      },
+      set a(val) {
+        params.arr[3] = val;
+      },
+      get b() {
+        return params.arr[4];
+      },
+      set b(val) {
+        params.arr[4] = val;
+      },
+      get x() {
+        return params.arr[5];
+      },
+      set x(val) {
+        params.arr[5] = val;
+      },
+      get y() {
+        return params.arr[6];
+      },
+      set y(val) {
+        params.arr[6] = val;
+      },
+      get random() {
+        return params.arr[7];
+      },
+      set random(val) {
+        params.arr[7] = val;
+      },
+    };
+  }
+
+  // @TODO — these could apply scale etc according to min, max, etc from 
+  //         PlinkyParam
+  get value() {
+    return this.arr[0]
+  }
+  set value(val) {
+    this.arr[0] = val
+  }
+
+}
+
+
+class Patch {
+
+  constructor(patch) {
+    this.buffer = patch;
+    this.params = [];
+
+    // each parameter has 16 bytes;
+    // first 2 bytes are the value, then the 7 mod matrix amounts
+    EParams.forEach((param, index) => {
+      // We have 16 bytes that we're looking at
+      const len = 16;
+      // Index to start slicing at
+      const idx = index * len;
+      // We have 16 bytes in the ArrayBuffer that we want — create an Int16Array
+      // over those bytes
+      const arr = new Int16Array(patch, idx, len)
+      const plinkyParam = getParam(param) || { name: param, id: param };
+      this.params.push(new Param({
+        ...plinkyParam,
+        arr
+      }));
+    });
+
+    // bitfield
+    this.bitFieldUInt8 = new Uint8Array(patch, patch.byteLength - 16, 16)
+    this.bitFieldInt8 = new Int8Array(patch, patch.byteLength - 16, 16)
+  }
+
+  // @TODO — i'm not quite sure I understand what the setters for these should 
+  //         look like yet
+  get arp() { return (this.bitFieldUInt8[0] & 1) > 0; }
+  get latch() { return (this.bitFieldUInt8[0] & 2) > 0; }
+  get loopStart() { return this.bitFieldInt8[1]; }
+  get loopLength() { return this.bitFieldInt8[2]; }
+
+  updateBuffer(patch) {
+    // @todo - blat `patch` over the contents of this.buffer without reassigning this.buffer. 
+  }
+  
+  setData(data, byteOffset) {
+    // @todo - blat this data over the relevant bit of this.buffer
+  }
+  
+  
+
+
+}
+
+
+
 export function patch2JSON(patch) {
+
+  window.patchBUFF = patch
+  
+  return new Patch(patch)
+
   let patchJSON = {
+    buffer: patch,
     arp: false,
     latch: false,
     loopStart: 0,
@@ -23,29 +140,14 @@ export function patch2JSON(patch) {
     // Index to start slicing at
     const idx = index * len;
     // We have 16 bytes in the ArrayBuffer that we want
-    const buf = patch.slice(idx, len*index+len);
-    // Then convert it to an Int16Array to get range of -1024 to 1024
-    // without having to do messy bit operations by hand! woot!
-    const arr = new Int16Array(buf);
+    const buf = patch.slice(idx, len * index + len);
+
     //console.log(param, len, idx, "BUF", buf, arr);
-    const plinkyParam = getParam(param) || { name: param };
-    patchJSON.params.push({
-      id:           param,
-      name:         plinkyParam.name,
-      description:  plinkyParam.description,
-      buffer:       buf,
-      value:        arr[0],
-      enum:         null,
-      mods: {
-        env:        arr[1],
-        pressure:   arr[2],
-        a:          arr[3],
-        b:          arr[4],
-        x:          arr[5],
-        y:          arr[6],
-        random:     arr[7],
-      }
-    });
+    const plinkyParam = getParam(param) || { name: param, id: param };
+    patchJSON.params.push(new Param({
+      ...plinkyParam,
+      buffer: buf
+    }));
   });
 
   //
@@ -62,10 +164,10 @@ export function patch2JSON(patch) {
   // - reserved for future use
   //
 
-  const field = patch.slice(patch.byteLength-16, patch.byteLength);
-  patchJSON.arp        = (new Uint8Array(field)[0] & 1) > 0;
-  patchJSON.latch      = (new Uint8Array(field)[0] & 2) > 0;
-  patchJSON.loopStart  = new Int8Array(field)[1];
+  const field = patch.slice(patch.byteLength - 16, patch.byteLength);
+  patchJSON.arp = (new Uint8Array(field)[0] & 1) > 0;
+  patchJSON.latch = (new Uint8Array(field)[0] & 2) > 0;
+  patchJSON.loopStart = new Int8Array(field)[1];
   patchJSON.loopLength = new Int8Array(field)[2];
 
   console.log('bitfield', field, patchJSON);
